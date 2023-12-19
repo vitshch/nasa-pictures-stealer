33a07c3a7d171b122fb41bd89ec250a342de74e6
vitalish <vitalish@mac.local>
2023-12-19 20:58:54 +0200

    Fix MR comment

--- src/main/java/com/bobocode/nasa/service/NasaPicturesStealerService.java
+++ src/main/java/com/bobocode/nasa/service/NasaPicturesStealerService.java
@@ -5,36 +5,45 @@ import com.bobocode.nasa.dto.nasa.PhotoDto;
 import com.bobocode.nasa.model.Camera;
 import com.bobocode.nasa.model.Photo;
 import com.bobocode.nasa.repository.CameraRepository;
-import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
-@Transactional
 public class NasaPicturesStealerService {
 
     private static final Logger LOG = LoggerFactory.getLogger(NasaPicturesStealerService.class);
 
     private final NasaPhotoService nasaPhotoService;
     private final CameraRepository cameraRepository;
+    private final TransactionTemplate transactionTemplate;
 
-    public NasaPicturesStealerService(NasaPhotoService nasaPhotoService, CameraRepository cameraRepository) {
+    public NasaPicturesStealerService(
+            NasaPhotoService nasaPhotoService,
+            CameraRepository cameraRepository,
+            TransactionTemplate transactionTemplate
+    ) {
         this.nasaPhotoService = nasaPhotoService;
         this.cameraRepository = cameraRepository;
+        this.transactionTemplate = transactionTemplate;
     }
 
     public void stealNasaPictures(int sol) {
         List<PhotoDto> nasaMarsPictures = nasaPhotoService.getNasaMarsPictures(sol);
         LOG.debug("Fetched {} photos from nasa by sol {}", nasaMarsPictures.size(), sol);
+        transactionTemplate.execute(status -> saveNasaPictures(nasaMarsPictures, status));
+    }
 
+    private TransactionStatus saveNasaPictures(List<PhotoDto> nasaMarsPictures, TransactionStatus transactionStatus) {
         nasaMarsPictures.stream()
                 .collect(Collectors.groupingBy(PhotoDto::camera))
                 .forEach(this::mapCamera);
+        return transactionStatus;
     }
 
     private Camera mapCamera(CameraDto cameraDto, List<PhotoDto> photoDtos) {
